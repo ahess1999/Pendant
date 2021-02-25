8db13606a73c39e03f0d5b03e0b29a3af602c9cb
Austin Hess <ahess@ksu.edu>
2021-02-25 01:13:01 -0600

    Added unit testing ability for comments

---
+++ CommentsAnalyzerTests/CommentsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Pendant;
+using TestHelper;
+
+namespace CommentsAnalyzerTests
+{
+    [TestClass]
+    public class CommentsTests : DiagnosticVerifier
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
