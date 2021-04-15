dd0562d4451eb1f5ebb55e20a9065e36a02155bc
Austin Hess <ahess@ksu.edu>
2021-04-14 21:57:49 -0500

    Added reference to the TutorWindow project

--- Pendant/CommentsAnalyzer.cs
+++ Pendant/CommentsAnalyzer.cs
@@ -14,7 +14,7 @@ namespace Pendant
         /// <summary>
         /// The Id for the diagnostic
         /// </summary>
-         public const string DiagnosticId = "Comments";
+        public const string DiagnosticId = "Comments";
 
         /// <summary>
         /// Title that displays when a diagnostic error is found
@@ -31,11 +31,10 @@ namespace Pendant
         /// </summary>
         internal const string Category = "CommentsAnalyzer Category";
 
-
         /// <summary>
         /// Creates the rule for the diagnostic
         /// </summary>
-         internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
 
         /// <summary>
         /// An immutable array of the diagnostics that returns a new ImmutableArray with the new rule added
